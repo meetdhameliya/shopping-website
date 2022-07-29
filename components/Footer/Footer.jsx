6d745e29811386af80f08ad96e75c8eead259ac4
meetdhameliya <88376029+meetdhameliya@users.noreply.github.com>
2022-07-30 01:22:59 +0530

    Add files via upload

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import { makeStyles } from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: "#FAFBFC",
+    padding: "30px 0px 40px 0px",
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+  },
+  content: {
+    display: "inline-flex",
+    marginTop: "4px",
+    justifyContent: "center",
+  },
+  link: {
+    textDecoration: "none",
+    margin: "0px 4px",
+  },
+}));
+
+export function Footer(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div>
+        This is made for educational purpose only and design is inspired by
+        <a href="https://www.myntra.com/" className={classes.link}>
+          Myntra.
+        </a>
+        Images used here are taken from
+        <a href="https://unsplash.com/" className={classes.link}>
+          Unsplash
+        </a>{" "}
+      </div>
+
+     
+    </div>
+  );
+}
